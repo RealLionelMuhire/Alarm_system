f1ea219b75669d4020352ace485eccb153d93b28
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-07 17:17:12 +0200

    modifying making threading more precise to avoid multiple threading running

--- beep_gen.py
+++ beep_gen.py
@@ -7,32 +7,45 @@ import wave
 
 audio_playing = False
 
-def play_sound(frequency, duration):
+
+def play_beep():
     global audio_playing
-    
-    p = pyaudio.PyAudio()
 
-    stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=True)
+    def play_sound(frequency, duration):
 
-    t = np.linspace(0, duration, int(44100 * duration), endpoint=False)
-    audio_sig = 0.5 * np.sin(2 * np.pi * frequency * t)
+        p = pyaudio.PyAudio()
 
-    for _ in range(5):#playing 5 consecutive beeps
-        if keyboard.is_pressed('t'):
-            break
-        else:
-            print("ALARM")
-            stream.write(audio_sig.tobytes())
+        stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=True)
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    audio_playing = False
+        t = np.linspace(0, duration, int(44100 * duration), endpoint=False)
+        audio_sig = 0.5 * np.sin(2 * np.pi * frequency * t)
 
-if __name__ == "__main__":
+        for _ in range(5):
+            if keyboard.is_pressed('t'):
+                break
+            elif not audio_playing:
+                print("ALARM")
+                stream.write(audio_sig.tobytes())
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        audio_playing = False
+
     print("Press 't' to stop")
     audio_thread = threading.Thread(target=play_sound, args=(2400, 1))
-    audio_thread.start()
-    keyboard.wait('t')
-    audio_thread.join()
-    print("Beep stopped.")
+    while True:
+        if not audio_playing and not audio_thread.is_alive():
+            audio_thread = threading.Thread(target=play_sound, args=(2400, 1))
+            audio_thread.start()
+            audio_playing = True
+
+        if keyboard.is_pressed('t'):
+            audio_playing = False
+            audio_thread.join()
+            print("Beep stopped.")
+            break
+
+
+if __name__ == "__main__":
+    play_beep()
