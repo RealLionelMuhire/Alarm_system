32b968e2c6463c16c48707ee3d36311b85bd0f5c
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-09 00:37:16 +0200

    adding something interesting

---
+++ frontend/src/Details.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Details.css";
+import ReactPlayer from "react-player";
+
+function DetailsPage({ onBackClicked, url, date }) {
+  return (
+    <div className="details-page">
+      <button onClick={onBackClicked} className="back-btn">
+        Back
+      </button>
+      <h3>Person Motion Detected</h3>
+      <ReactPlayer url={url} controls={true} />
+      <p>{date}</p>
+    </div>
+  );
+}
+
+export default DetailsPage;
